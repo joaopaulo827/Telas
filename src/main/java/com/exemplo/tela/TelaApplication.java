e8cb1599c40f550b355dc1d7aec320409e70bf16
joaopaulo827 <joaopauloterziotti@gmail.com>
2026-05-19 17:20:36 -0300

    teste

---
+++ src/main/java/com/exemplo/tela/TelaApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.tela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelaApplication.class, args);
+	}
+
+}
